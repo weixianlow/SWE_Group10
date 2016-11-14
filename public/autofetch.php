072077811da0a1b19ea065b2432ddad840c5e917
weixianlow <weix.low@gmail.com>
2016-11-14 14:56:54 -0600

    testing webhooks command

--- public/autofetch.php
+++ public/autofetch.php
@@ -1,5 +1,4 @@
 <?php
-shell_exec('cd /var/www/html/test-cs4320/SWE_Group10/');
-$output = shell_exec('sudo git pull downstream master');
-echo "<pre>$output</pre>";
+echo '<pre>'.shell_exec("cd /var/www/html/test_cs4320/SWE_Group10/").'</pre>';
+echo '<pre>'.shell_exec("sudo git pull downstream master").'</pre>';
 ?>
