83a8684ddced7f58953722c95e318069ac84bd8e
weixianlow <weix.low@gmail.com>
2016-11-14 14:34:27 -0600

    Relocating files to fix github functionality

--- public/dbtest.php
+++ public/dbtest.php
@@ -1,23 +1,26 @@
 <?php
+   // connect to mongodb
+   $m = new MongoClient();
+   echo "Connection to database successfully\n";
+	
+   // select a database
+   $db = $m->mydb;
+   echo "Database mydb selected";
+   $collection = $db->mycol;
+   echo "Collection selected succsessfully\n";
 
-$m = new MongoClient();
-echo "Connection to database successfully"
-
-$db = $m -> test;
-
-echo "database selected"
-$collection = $db -> testcase;
-echo "collection selected"
-
-$collection->update(array("title" => "MongoDB"),
-	array("$set'=>array("title"=>"MongoDB Tutorial")));
-echo "document updated successfully";
-
-$cursor = $collecton->find();
-
-echo "updated document";
-
-foreach($cursor as $document){
-	echo $document["title"] . "\n";
-	}
+   // now update the document
+   $collection->update(array("title"=>"MongoDB"), 
+      array('$set'=>array("title"=>"MongoDB Tutorial")));
+   echo "Document updated successfully\n";
+	
+   // now display the updated document
+   $cursor = $collection->find();
+	
+   // iterate cursor to display title of documents
+   echo "Updated document\n";
+	
+   foreach ($cursor as $document) {
+      echo $document["title"] . "\n";
+   }
 ?>
